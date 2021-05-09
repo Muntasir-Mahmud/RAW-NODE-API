7516a8c2989c2c53ee63d5161598df16969699d7
Muntasir Mahmud <m.siyam529@gmail.com>
2021-05-09 16:47:14 +0600

    Added a basic server and nodemon

--- index.js
+++ index.js
@@ -5,3 +5,30 @@
  * Date : 9/5/2021
  *
  */
+
+// dependencies
+const http = require('http');
+
+// app object - module scaffholding
+const app = {};
+
+// configuration
+app.config = {
+    port: 3000,
+};
+
+// create server
+app.createServer = () => {
+    const server = http.createServer(app.handleReqRes);
+    server.listen(app.config.port, () => {
+        console.log(`listening to the port ${app.config.port}`);
+    });
+};
+
+// handel Request Response
+app.handleReqRes = (req, res) => {
+    res.end('Hello siam');
+};
+
+// start the server
+app.createServer();
