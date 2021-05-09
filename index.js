3475f1f1279eb94aad39cb211c7724e2369829f5
Muntasir Mahmud <m.siyam529@gmail.com>
2021-05-09 16:25:37 +0600

    Setup the environment

---
+++ index.js
@@ -0,0 +1,7 @@
+/*
+ * Title : Uptime Monitoring Applicaction
+ * Description : A RESTful API to monitor up or down time of user defined links
+ * Author : Muntasir Mahmud
+ * Date : 9/5/2021
+ *
+ */
