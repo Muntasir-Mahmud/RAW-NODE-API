50ace76672793cbf9dd188939204b98b6a370eb8
Muntasir Mahmud <m.siyam529@gmail.com>
2021-05-09 23:00:44 +0600

    Setup Routing

--- helpers/handleReqRes.js
+++ helpers/handleReqRes.js
@@ -1,6 +1,8 @@
 // dependencies
 const url = require('url');
 const { StringDecoder } = require('string_decoder');
+const routes = require('../routes');
+const { notFoundHandler } = require('../handlers/routeHandlers/notFoundHandler');
 
 // module scaffolding
 const handler = {};
@@ -15,8 +17,30 @@ handler.handleReqRes = (req, res) => {
     const querystringObject = parsedUrl.query;
     const headerObject = req.headers;
 
+    const requestProperties = {
+        parsedUrl,
+        path,
+        trimmedPath,
+        method,
+        querystringObject,
+        headerObject,
+    };
+
     const decoder = new StringDecoder('utf-8');
     let realData = '';
+
+    const chosenHandler = routes[trimmedPath] ? routes[trimmedPath] : notFoundHandler;
+
+    chosenHandler(requestProperties, (statusCode, payload) => {
+        statusCode = typeof statusCode === 'number' ? statusCode : 500;
+        payload = typeof payload === 'object' ? payload : {};
+
+        const payloadString = JSON.stringify(payload);
+
+        // return the final response
+        res.writeHead(statusCode);
+        res.end(payloadString);
+    });
 
     req.on('data', (buffer) => {
         realData += decoder.write(buffer);
@@ -28,7 +52,6 @@ handler.handleReqRes = (req, res) => {
         console.log(realData);
     });
     // response handle
-    res.end('Hello siam');
 };
 
 module.exports = handler;
