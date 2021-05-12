3cb845e1214c09aad95ae58435070c7eecbfdcae
Muntasir Mahmud <m.siyam529@gmail.com>
2021-05-13 02:38:29 +0600

    Added environment variables

--- index.js
+++ index.js
@@ -9,20 +9,16 @@
 // dependencies
 const http = require('http');
 const { handleReqRes } = require('./helpers/handleReqRes');
+const environment = require('./helpers/environments');
 
 // app object - module scaffholding
 const app = {};
 
-// configuration
-app.config = {
-    port: 3000,
-};
-
 // create server
 app.createServer = () => {
     const server = http.createServer(app.handleReqRes);
-    server.listen(app.config.port, () => {
-        console.log(`listening to the port ${app.config.port}`);
+    server.listen(environment.port, () => {
+        console.log(`listening to the port ${environment.port}`);
     });
 };
 
