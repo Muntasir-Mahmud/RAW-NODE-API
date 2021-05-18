103909c8ef2016b188fd8a874b4b927910bbbae2
Muntasir Mahmud <m.siyam529@gmail.com>
2021-05-19 04:07:10 +0600

    Added Create Token functionality

--- handlers/routeHandlers/tokenHandler.js
+++ handlers/routeHandlers/tokenHandler.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable prettier/prettier */
 // dependencies
-// const { hash } = require('../../helpers/utilities');
-// const { parseJSON } = require('../../helpers/utilities');
+const { createRandomString, parseJSON } = require('../../helpers/utilities');
+const { hash } = require('../../helpers/utilities');
 const data = require('../../lib/data');
 
 // module scaffolding
@@ -20,18 +20,59 @@ handler.tokenHandler = (requestProperties, callback) => {
 handler._token = {};
 
 handler._token.post = (requestProperties, callback) => {
+    const phone = typeof requestProperties.body.phone === 'string'
+        && requestProperties.body.phone.trim().length === 11
+            ? requestProperties.body.phone
+            : false;
 
+    const password = typeof requestProperties.body.phone === 'string'
+        && requestProperties.body.phone.trim().length > 0
+            ? requestProperties.body.phone
+            : false;
+    if (phone && password) {
+        data.read('users', phone, (err1, userData) => {
+            const hashPassword = hash(password);
+            if (hashPassword === parseJSON(userData).password) {
+                const tokenId = createRandomString(20);
+                const expires = Date.now() + 60 * 60 * 1000;
+                const tokenObject = {
+                    phone,
+                    id: tokenId,
+                    expires,
+                };
+
+                // store the token
+                data.create('tokens', tokenId, tokenObject, (err2) => {
+                    if (!err2) {
+                        callback(200, tokenObject);
+                    } else {
+                        callback(500, {
+                            error: 'There was a problem in the server side!',
+                        });
+                    }
+                });
+            } else {
+                callback(400, {
+                    error: 'Password is not valid',
+                });
+            }
+        });
+    } else {
+        callback(400, {
+            error: 'You have problem in your request',
+        });
+    }
 };
 
-handler._token.get = (requestProperties, callback) => {
+// handler._token.get = (requestProperties, callback) => {
 
-};
+// };
 
-handler._token.put = (requestProperties, callback) => {
+// handler._token.put = (requestProperties, callback) => {
 
-};
+// };
 
-handler._token.delete = (requestProperties, callback) => {
+// handler._token.delete = (requestProperties, callback) => {
 
-};
+// };
 module.exports = handler;
--- helpers/utilities.js
+++ helpers/utilities.js
@@ -39,5 +39,25 @@ utilities.hash = (hashString) => {
     return false;
 };
 
+// Create random string
+utilities.createRandomString = (strlength) => {
+    let length = strlength;
+    length = typeof strlength === 'number' && strlength > 0 ? strlength : false;
+
+    if (length) {
+        const possiblecharacters = 'abcdefghijklmnopqrstuvwxyz1234567890';
+        let output = '';
+        for (let i = 1; i <= length; i += 1) {
+            const randomCharacter = possiblecharacters.charAt(
+                // eslint-disable-next-line prettier/prettier
+                Math.floor(Math.random() * possiblecharacters.length),
+            );
+            output += randomCharacter;
+        }
+        return output;
+    }
+    return false;
+};
+
 // export module
 module.exports = utilities;
